e9d20af60839372cf4da479a869d0896d44e585c
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-05-18 12:30:01 +0200

    Skapat interface för kurser

--- src/app/models/courses.ts
+++ src/app/models/courses.ts
@@ -1,1 +1,13 @@
-export interface Courses {}
+import { compileDeclareClassMetadata } from "@angular/compiler";
+
+export interface Course{
+  courseCode: string;
+  courseName: string;
+  points: number;
+  progression: string;
+  subject: string;
+  syllabus: string;
+
+}
+
+//kod, kursnamn, poäng, nivå, ämne, kursplan
