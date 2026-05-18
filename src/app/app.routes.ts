e561441c8dab25b6d96b68d9f14d0c5cba3a72d7
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-05-18 11:07:41 +0200

    Lägger till routes, models och pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Myschedule } from './pages/myschedule/myschedule';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: Home},
+  { path: "", component: Myschedule}
+];
---
+++ src/app/pages/myschedule/myschedule.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myschedule',
+  imports: [],
+  templateUrl: './myschedule.html',
+  styleUrl: './myschedule.css',
+})
+export class Myschedule {}
