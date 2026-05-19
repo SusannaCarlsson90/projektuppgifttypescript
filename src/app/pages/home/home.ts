78af39e4d7240943498f00d7163215a68a9c36a3
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-05-19 15:28:55 +0200

    lagt in kod från ett tidigare projekt där jag hämtat ett API med ramschema och korrigerat koden för att anpassa till detta projekt.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit, computed } from '@angular/core';
+import { Course } from '../../models/courses'; // Mitt interface
+import { CourseService } from '../../services/course'; // Min service course.ts
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class HomeComponent implements OnInit { //Skapar tre signalar: courses, error och serchTerm. 
+ courses = signal<Course[]>([]);
+  error = signal<string | null>(null);
+  searchTerm = signal<string>('')// En behållare som sparar texten användaren skriver i sökrutan
+
+  filteredCourses = computed(() => { //Computed skapar en ny, smart lista som håller koll på de andra signalerna. 
+    const term = this.searchTerm().toLowerCase(); // Hämtar sökordet och gör om till små bokstäver oavsett hur användaren skriver
+    const list = this.courses(); // Hämtar alla kurser 
+
+    // Returnerar bara de kurser som matchar sökordet i kod eller namn
+    return list.filter(course => //Filtrerar listan och behåller bara det som matchar vad användaren skriver
+      course.courseCode.toLowerCase().includes(term) || 
+      course.courseName.toLowerCase().includes(term)
+    );
+  });
+  
+  courseService = inject(CourseService); //Kopplar på kurs-service
+  ngOnInit() {
+  this.loadCourses();
+  }
+
+//Funktion som anropar servicen och sparar kurserna i min signal
+  loadCourses() { this.courseService.getCourses().subscribe({
+    next:(response: any) => {
+      this.courses.set(response);
+      console.table(this.courses());
+    },
+    error: (err: any) => {
+      console.error(err);
+      this.error.set("Kunde inte ladda kurserna, försök igen senare");
+    }
+  });
+}
+//Metod för sökning
+  handleSearch(event: Event) {
+    const input = event.target as HTMLInputElement; //Meddelar att det är en textruta
+    this.searchTerm.set(input.value.toLowerCase()); //Sparar det som skrivs och gör om till små bokstäver
+  }
+
+
+// En funktion som körs när användaren klickar på en rubrik för att sortera listan
+sortData(key: keyof Course) {
+  const sorted = [...this.courses()].sort((a, b) => { // Sortera i bokstavsordning
+    return String(a[key]).localeCompare(String(b[key])); // Jämför a mot b
+  });
+
+  // Om listan redan är sorterad stigande, vänd på den så den blir fallande
+  if (JSON.stringify(this.courses()) === JSON.stringify(sorted)) {
+    sorted.reverse();
+  }
+
+  this.courses.set(sorted);
+}
+}
--- src/app/services/course.ts
+++ src/app/services/course.ts
@@ -16,7 +16,7 @@ export class CourseService {
   private jsonUrl = '/miun_courses.json'; 
 
   //En funktion som startar en hämtning av min JSON-fil 
-  getCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>(this.jsonUrl);
+  getCourses(): Observable<Course[]> { //// Talar om att vi vill hämta en lista med kurser
+    return this.http.get<Course[]>(this.jsonUrl);// Hämtar filen och kontrollerar att den följer reglerna i mitt interface (Course)
   }
-}
+  }
