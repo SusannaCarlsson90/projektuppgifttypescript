80b47721a6507d8866b1dbaf55343bdb3b6b72da
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-05-20 16:08:17 +0200

    uppdaterat myschedule.html samt lagt på knapp för lägg till på home.html

--- src/app/pages/myschedule/myschedule.ts
+++ src/app/pages/myschedule/myschedule.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ScheduleService } from '../../services/schedule.service';
 
 @Component({
   selector: 'app-myschedule',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './myschedule.html',
   styleUrl: './myschedule.css',
 })
-export class Myschedule {}
+export class Myschedule {
+  scheduledCourses = inject(ScheduleService).courses;
+}
