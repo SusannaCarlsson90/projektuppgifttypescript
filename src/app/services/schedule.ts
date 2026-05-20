a7f0cf9160d34adb5c63a6d74cbc4092eeaf2533
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-05-20 13:18:47 +0200

    Skapat service för att spara kurser

--- src/app/services/schedule.ts
+++ src/app/services/schedule.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
+import { Course } from '../models/courses';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Schedule {}
+export class Schedule {
+  private scheduledCourses = signal<Course[]>([]); //Sparar listan med valda kurser när användaren ska skapa sitt eget schema
+  courses = this.scheduledCourses.asReadonly();
+}
