b012017af588c1555181abad27fa711d15fa4580
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-06-05 17:04:38 +0200

    rensat kod

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { Navbar } from './components/navbar/navbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar], // Lägg till Navbar här
+  imports: [RouterOutlet, Navbar], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
